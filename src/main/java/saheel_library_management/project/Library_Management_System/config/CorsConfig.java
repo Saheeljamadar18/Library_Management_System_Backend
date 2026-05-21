0880bddaf883ee8b747df1cead6b5eaa6121b4d2
Saheel Jamadar <your@email.com>
2026-05-21 11:52:00 +0530

    Add Vercel URL to CORS config

--- src/main/java/saheel_library_management/project/Library_Management_System/config/CorsConfig.java
+++ src/main/java/saheel_library_management/project/Library_Management_System/config/CorsConfig.java
@@ -14,7 +14,11 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000"));
+        config.setAllowedOrigins(List.of(
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "https://student-library-management-system-frontend.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
